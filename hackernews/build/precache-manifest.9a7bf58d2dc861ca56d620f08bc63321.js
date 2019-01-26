c6c38eb87680618e311fbc675f43adecb362b44c
M <faymatt96@gmail.com>
2019-01-25 20:54:33 -0500

    changes

---
+++ hackernews/build/precache-manifest.9a7bf58d2dc861ca56d620f08bc63321.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1602332b801076df13",
+    "url": "/hackernews/static/js/runtime~main.da160233.js"
+  },
+  {
+    "revision": "d2ceac80913ea95c3edb",
+    "url": "/hackernews/static/js/main.d2ceac80.chunk.js"
+  },
+  {
+    "revision": "4e4e3f49b7635b8a059e",
+    "url": "/hackernews/static/js/1.4e4e3f49.chunk.js"
+  },
+  {
+    "revision": "d2ceac80913ea95c3edb",
+    "url": "/hackernews/static/css/main.a321c1dd.chunk.css"
+  },
+  {
+    "revision": "80ac0f0c53dd5a342bc6905b77381a21",
+    "url": "/hackernews/index.html"
+  }
+];
