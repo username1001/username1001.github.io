2004878b626a854ad724d736a756fbc0f4ed89ee
M <faymatt96@gmail.com>
2019-01-25 19:56:16 -0500

    Add hackernews clone

---
+++ hackernews/src/App.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
+// const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY
+    };
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
+    const updatedHits = [...oldHits, ...hits];
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
+  }
+
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({
+      result: { ...this.state.result, hits: updatedHits }
+    });
+  }
+
+  render() {
+    const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}
+          >
+            More
+          </Button>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
+        </div>
+        {result && <Table list={result.hits} onDismiss={this.onDismiss} />}
+      </div>
+    );
+  }
+}
+
+const Search = ({ value, onChange, onSubmit, children }) => (
+  <form onSubmit={onSubmit}>
+    <input type="text" value={value} onChange={onChange} />
+    <button type="submit">{children}</button>
+  </form>
+);
+
+const Table = ({ list, onDismiss }) => (
+  <div className="table">
+    {list.map(item => (
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    ))}
+  </div>
+);
+
+const Button = ({ onClick, className = '', children }) => (
+  <button onClick={onClick} className={className} type="button">
+    {children}
+  </button>
+);
+
+export default App;
---
+++ hackernews/src/list.js
@@ -0,0 +1,84 @@
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com//redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+  {
+    title: 'Node',
+    url: 'https://github.com/reactjs/',
+    author: 'Someone',
+    num_comments: 2,
+    points: 5,
+    objectID: 2
+  },
+  {
+    title: 'JavaScript',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Brendan Eich',
+    num_comments: 2,
+    points: 5,
+    objectID: 3
+  },
+  {
+    title: 'Python',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Brendan Eich',
+    num_comments: 2,
+    points: 5,
+    objectID: 4
+  },
+  {
+    title: 'C',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Brendan Eich',
+    num_comments: 2,
+    points: 5,
+    objectID: 5
+  },
+  {
+    title: 'Java',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Brendan Eich',
+    num_comments: 2,
+    points: 5,
+    objectID: 6
+  },
+  {
+    title: 'C++',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Brendan Eich',
+    num_comments: 2,
+    points: 9,
+    objectID: 7
+  },
+  {
+    title: 'C#',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Brendan Eich',
+    num_comments: 2,
+    points: 5,
+    objectID: 8
+  },
+  {
+    title: 'TypeScript',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Brendan Eich',
+    num_comments: 2,
+    points: 5,
+    objectID: 9
+  }
+];
+
+module.exports = list;
